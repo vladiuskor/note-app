b0168f3ad007b986cc0fcf08ec478e8c1a2ab728
Vlad <max.vlad.kor@gmail.com>
2021-08-07 20:27:50 +0300

    Added application code.

---
+++ notes.js
@@ -0,0 +1,83 @@
+const fs = require('fs');
+const chalk = require('chalk');
+
+const getNotes = () => 'Your notes ...';
+
+
+const addNote = (title, body) => {
+    const notes = loadNotes();
+    const duplicateNote = notes.find((note) => note.title === title)
+
+    if(!duplicateNote) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes);
+        console.log(chalk.green.inverse('New note added!'));
+    } else {
+        console.log(chalk.red.inverse('Note title taken!'));
+    }
+
+}
+
+
+const removeNote = title => {
+    const notes = loadNotes();
+    const notesToKeep= notes.filter(note => note.title !== title);
+
+    if(notes.length > notesToKeep.length){
+        console.log(chalk.green.inverse('Note removed!'));
+    } else {
+        console.log(chalk.red.inverse('No note  found!'));
+    }
+
+    saveNotes(notesToKeep);
+}
+
+const listNotes = () => {
+    const notes = loadNotes();
+
+    console.log(chalk.cyan.inverse('Your notes:'));
+    notes.forEach(note => console.log(`${chalk.cyan.bold(note.title)}: ${note.body};`));
+
+}
+
+const readNote = title => {
+    const notes = loadNotes();
+    const note = notes.find((note) => note.title === title);
+
+    if(note) {
+      console.log(chalk.inverse(note.title));
+      console.log(chalk.inverse(note.body));
+    } else {
+        console.log(chalk.red.inverse('Note not found!'));
+    }
+}
+
+
+const saveNotes = notes => {
+    const dataJSON = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', dataJSON);
+}
+
+
+const loadNotes = () => {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json', 'utf-8')
+        return JSON.parse(dataBuffer);
+    } catch (e) {
+        return [];
+    }
+
+}
+
+
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote,
+    readNote: readNote,
+    listNotes: listNotes
+};
